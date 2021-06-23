137b322bf8d7c41fafcf73a1f20011781ae98211
rrogers422 <Reagan422440@gmail.com>
2021-06-22 20:06:24 -0400

    routes for visit page/returns to original page if none found

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,22 @@
+//Dependencies
 const express = require("express");
 const path = require("path");
 const fr = require("fs");
+const { Router } = require("express");
 
+//Starting the Express app on port 3000
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+//these routes handle "visit" page
+app.GET('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.GET('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
